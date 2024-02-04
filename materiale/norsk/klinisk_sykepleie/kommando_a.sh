6dda22bb3350171e4e870d3d375885015a8fee60
Trond Trosterud <trond.trosterud@uit.no>
2024-02-04 20:58:50 +0100

    korrigert

--- materiale/norsk/klinisk_sykepleie/kommando_a.sh
+++ materiale/norsk/klinisk_sykepleie/kommando_a.sh
@@ -1,6 +1,4 @@
-cat $GTLANGS/term-sme-x-nursing-private/materiale/norsk/klinisk_sykepleie/klinisk?.txt |\
-hfst-tokenise -cg $GTLANGS/lang-nob/tools/tokenisers/tokeniser-disamb-gt-desc.pmhfst |\
-vislcg3 -g $GTLANGS/lang-nob/src/cg3/disambiguator.cg3 |\
+cat klinisk.dis |\
 grep -v '^[":]'|\
 grep '" A '|\
 cut -d'"' -f2|\
@@ -11,15 +9,16 @@ uniq|\
 grep -v '[A-ZÆØÅ0-9,:;()]'|\
 grep -v '^.$'|\
 grep -v '^$'|\
-lookup $GTHOME/words/dicts/nobsme/bin/nobsme-all.fst|\
+lookup $GTLANGS/dict-nob-sme/bin/nobsme-all.fst|\
 grep "+?"|\
 cut -f1|\
 sort|\
 uniq -c|\
 sort -nr|\
 cut -c6-|\
-lookup $GTHOME/words/dicts/nobfin/bin/nobfin-all.fst|\
+lookup $GTLANGS/dict-nob-fin/bin/nobfin-all.fst|\
 grep "+?"|\
+sed 's/$/\tA\t/' |\
 cut -f1 > klinisk_a_missing.freq
 
 
